3b769342d1ecef983bad32064e042efdee91a800
milanvarady <milanvarady@users.noreply.github.com>
2025-11-14 11:59:00 +0000

    deploy: bb379a12d024b1dad0e0bc7fa1e9cd7eff425ac6

--- hash__table__core_8c.js
+++ hash__table__core_8c.js
@@ -2,7 +2,7 @@ var hash__table__core_8c =
 [
     [ "hash_table_create", "hash__table__core_8c.html#a145041ed407574baae7b02482a62bcac", null ],
     [ "hash_table_delete", "hash__table__core_8c.html#a085d39918c30cf7ca3724c1f14a17bcd", null ],
-    [ "hash_table_destroy", "hash__table__core_8c.html#a5615fc731a05f4480e78bd98e3a280c1", null ],
+    [ "hash_table_destroy", "hash__table__core_8c.html#a0bade541b10532f274e5ee78746e6196", null ],
     [ "hash_table_foreach", "hash__table__core_8c.html#a8b5f3fafdfff65f0c1a36c02ea712ca4", null ],
     [ "hash_table_get", "hash__table__core_8c.html#a8d53f49b5d48520f25043c0e1e77f9cc", null ],
     [ "hash_table_insert", "hash__table__core_8c.html#ac3c9737f9417f37f807ab6c03b00d1d9", null ]
